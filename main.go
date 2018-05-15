6eac7bc20646149cb15073e4759be0f35013dfad
GTimur <to-timur@yandex.ru>
2018-05-15 14:46:26 +0300

    task 02

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"memogo"
-	"time"
-)
-
-// REMIND SCENARIO
-// REPEAT_UNTIL_EVENT_START = Remind in Int1 before event DateStart and repeat remind every Int2 until event DateStart
-// REPEAT_UNTIL_EVENT_END = Remind from DateStart to DateEnd with repeat inteval=Int2 (minutes)
-
-// Remind options
-type Remind struct {
-	DateStart time.Time //Date start of event
-	DateEnd   time.Time //Date end of event
-	Int1      int       //Frequency of reminder messages (minutes)
-	Int2      int       //Frequency of reminder messages (minutes)
-}
-
-// Memo definiton
-type Memo struct {
-	ID       int       //ID
-	Date     time.Time //Date of creation
-	Reminder Remind    //Remind scenario options
-	Subj     string    //visible memo subject
-	Memo     []string  //memo body
-	Mails    []string  //emails list
-}
-
-var globalconfig memogo.Config
-
 func main() {
-	memo := Memo{}
-	rem := Remind{}
-	//smtp :=
-	globalconfig = memogo.Config{
+	memogo.GlobalConfig = memogo.Config{
 		Root: "./root/",
 		SMTPSrv: memogo.SrvSMTP{
 			Addr:     "10.20.20.6",
@@ -51,57 +17,32 @@ func main() {
 			Port: 8000,
 		},
 	}
+	
 
-	var files map[string]string
-	var dirs map[string]string
+	//var files map[string]string
 
-	files = make(map[string]string)
-	dirs = make(map[string]string)
+	//files, err := memogo.FindAllFiles(globalconfig.Root, []string{"*.*"})
+	//if err != nil {
+	//	log.Fatalf("Main(): FindFiles error: %v", err)
+	//}
 
-	loc, err := time.LoadLocation("Europe/Moscow")
+	//groupname := path.Dir(strings.Replace("./root/test2/rule01.json", globalconfig.Root, "", -1))
+	//fmt.Println("GROUPS FOUND:", groupname)
+	//fmt.Println("FILES FOUND:", files)
+
+	/*err = GlobalConfig.MakeConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	rem.DateStart = time.Date(2018, time.May, 11, 10, 0, 0, 0, loc)
-	rem.DateEnd = time.Date(2018, time.August, 16, 23, 59, 59, 0, loc)
-	rem.Int1 = 5 //in * minutes
-	rem.Int2 = 1 //every * minutes
-
-	memo.ID = 100
-	memo.Date = time.Now()
-	memo.Subj = "Сертификат thawte ibank.ymkbank.ru истекает 17.08.2018"
-	memo.Memo = []string{"Истекает срок действия сертификата для", "web-сервера ibank.ymkbank.ru"}
-	memo.Mails = []string{"gtg@ymkbank.ru"}
-	memo.Reminder = rem
-
-	fmt.Println(memo)
-	dirs, err = memogo.FindFiles(globalconfig.Root, []string{"*"})
-	if err != nil {
-		log.Fatalf("Main(): FindFiles error: %v", err)
-	}
-	fmt.Println("FILES FOUND:", dirs)
-
-	for k, _ := range dirs {
-		f, err := memogo.FindFiles(k, []string{"*.*"})
-		if err != nil {
-			log.Fatalf("Main(): FindFiles error: %v", err)
-		}
-		fmt.Println("FILES FOUND:", f)
-
-		for kk, vv := range f {
-			files[kk] = vv
-		}
-	}
-
-	fmt.Println("FILES FOUND:", files)
-
-	err = globalconfig.MakeConfig()
+	err = GlobalConfig.WriteJSON()
 	if err != nil {
 		panic(err)
 	}
-	err = globalconfig.WriteJSON()
+
+	err = memogo.TestJSON()
 	if err != nil {
-		panic(err)
-	}
+		log.Fatalf("TestJSON() error: %v", err)
+	}*/
+
 }
