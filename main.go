32e4f9973ed62db421d9abc0b9af936d74106b86
GTimur <to-timur@yandex.ru>
2018-08-10 22:57:44 +0300

    SendMemo 1

--- main.go
+++ main.go
@@ -12,13 +12,19 @@ import (
 )
 
 func main() {
-	memogo.GlobalConfig = memogo.Config{
+	var err error
+	err = memogo.GlobalConfig.ReadJSON()
+	if err != nil {
+		log.Fatalln("Error: Config file (", memogo.CONFIGFILE, ") not found.\n Program terminated.")
+	}
+
+	/*memogo.GlobalConfig = memogo.Config{
 		Root: "root\\",
 		SMTPSrv: memogo.SrvSMTP{
 			Addr:     "10.20.20.6",
 			Port:     25,
 			Account:  "noti",
-			Password: "Bank999",
+			Password: "Password111",
 			From:     "noti@ymkbank.ru",
 			FromName: "Memo GO",
 			UseTLS:   false,
@@ -28,6 +34,8 @@ func main() {
 			Port: 8000,
 		},
 	}
+	memogo.GlobalConfig.WriteJSON()
+	*/
 
 	// Инициализация web-сервера
 	memogo.NeedExit = false // флаг для завершения работы
@@ -38,7 +46,7 @@ func main() {
 	fmt.Println("Web control configured: " + "http://" + memogo.GlobalConfig.ManagerSrvAddr() + ":" + strconv.Itoa(int(memogo.GlobalConfig.ManagerSrvPort())))
 
 	/* Запускаем сервер обслуживания WebCtl */
-	err := web.StartServe()
+	err = web.StartServe()
 	if err != nil {
 		log.Println("HTTP сервер: Ошибка. ", err)
 		os.Exit(1)
@@ -59,7 +67,34 @@ func main() {
 	ticker := time.NewTicker(time.Second * 1) // Запускаем обработчик каждую секунду
 
 	// Зациклимся с таймером посекундно пока не получим команду завершения работы.
+	h := 0
 	for range ticker.C {
+		err := memogo.MemoSvc(memogo.GlobalQueue)
+		if err != nil {
+			log.Println("MemoSvc error:", err)
+		}
+
+		// Опрос папки /root раз в час + переинициализация списков
+		if h == 60*60 {
+			err = memogo.TasksReload()
+			if err != nil {
+				log.Println("TasksReload error:", err)
+			}
+
+			// read GlobalTask array and rebuild GlobalTimeMap
+			err = memogo.BuildTimeMap()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// read GlobalTimeMap and build GlobalQueue
+			err = memogo.GlobalQueue.MakeQueue()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		h++
+
 		if !memogo.NeedExit {
 			continue
 		}
